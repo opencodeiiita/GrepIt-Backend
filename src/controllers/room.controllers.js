b02a7ff54f9fc65da0d008e3facde6870f0dbcd0
Sagnik Mandal <acriticalcynic@outlook.com>
2023-12-15 19:56:08 +0530

    updated room scheme & added removeUserFromRoom in the roomcontroller

--- src/controllers/room.controllers.js
+++ src/controllers/room.controllers.js
@@ -52,4 +52,73 @@ async function createRoom(req, res) {
     }
 }
 
-export { createRoom };
+async function removeUserFromRoom(req, res) {
+    try {
+        const { roomId, userId } = req.body;
+
+        const room = await prisma.room.findUnique({
+            where: {
+                id: roomId
+            },
+            include: {
+                users: true
+            }
+        });
+
+        if (!room) {
+            console.log('Error removing user from room: Room does not exist');
+            res.status(200).json({
+                error: 'Room does not exist'
+            });
+            return;
+        }
+
+        const user = await prisma.user.findUnique({
+            where: {
+                id: userId
+            }
+        });
+
+        if (!user) {
+            console.log('Error removing user from room: User does not exist');
+            res.status(400).json({
+                error: 'User does not exist'
+            });
+            return;
+        }
+
+        const userInRoom = room.users.find((user) => user.id === userId);
+        if (!userInRoom) {
+            console.log('Error removing user from room: User is not in the room');
+            res.status(400).json({
+                error: 'User is not in the room'
+            });
+            return;
+        }
+
+        const updatedRoom = await prisma.room.update({
+            where: {
+                id: roomId
+            },
+            data: {
+                users: {
+                    disconnect: {
+                        id: userId
+                    }
+                }
+            }
+        });
+
+        res.status(200).json({
+            message: 'User removed from room successfully',
+            room: updatedRoom
+        });
+    } catch (e) {
+        console.log(`Error removing user from room: ${e}`);
+        res.status(500).json({
+            error: 'Internal Server Error'
+        });
+    }
+}
+
+export {removeUserFromRoom, createRoom};
--- src/routes/v1/room.routes.js
+++ src/routes/v1/room.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { createRoom } from '../../controllers/room.controllers.js';
-import { fetchUser } from '../../middlewares/userAuth.middleware.js';
+import { fetchUser, removeUserFromRoom } from '../../middlewares/userAuth.middleware.js';
 
 const router = express.Router();
 
 router.route('/create').post(fetchUser, createRoom);
+router.route("/user/delete").post(removeUserFromRoom);
 
 export default router;
