e5f894a51744b8d64d415e8d360ed004ace9c3ca
Sagnik Mandal <acriticalcynic@outlook.com>
2023-12-16 18:16:17 +0530

    added addUserToRoom feature

--- src/routes/v1/room.routes.js
+++ src/routes/v1/room.routes.js
@@ -1,5 +1,10 @@
 import express from 'express';
-import { createRoom, removeUserFromRoom, updateRoom } from '../../controllers/room.controllers.js';
+import { 
+    createRoom,
+    removeUserFromRoom,
+    updateRoom,
+    addUserToRoom 
+} from '../../controllers/room.controllers.js';
 import { fetchUser } from '../../middlewares/userAuth.middleware.js';
 
 const roomRouter = express.Router();
@@ -7,5 +12,6 @@ const roomRouter = express.Router();
 roomRouter.route('/create').post(fetchUser, createRoom);
 roomRouter.route("/user/delete").post(fetchUser, removeUserFromRoom);
 roomRouter.route('/update').patch(fetchUser, updateRoom);
+roomRouter.route('/user/add').post(fetchUser, addUserToRoom);
 
 export default roomRouter;
