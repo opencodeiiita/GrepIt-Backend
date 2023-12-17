d1dbb85c23df27c2b7a8b58a77296eee0156bec2
Sagnik Mandal <acriticalcynic@outlook.com>
2023-12-17 20:55:19 +0530

    move authVerify one level down

--- src/routes/v1/room.routes.js
+++ src/routes/v1/room.routes.js
@@ -6,14 +6,13 @@ import {
     addUserToRoom,
     disconnectUserFromRoom
 } from '../../controllers/room.controllers.js';
-import { authVerify } from '../../middlewares/userAuth.middleware.js';
 
 const roomRouter = express.Router();
 
-roomRouter.route('/create').post(authVerify, createRoom);
-roomRouter.route('/user/remove').post(authVerify, removeUserFromRoom);
-roomRouter.route('/update').patch(authVerify, updateRoom);
-roomRouter.route('/user/add').post(authVerify, addUserToRoom);
-roomRouter.route('/user/disconnect').post(authVerify, disconnectUserFromRoom);
+roomRouter.route('/create').post(createRoom);
+roomRouter.route('/user/remove').post(removeUserFromRoom);
+roomRouter.route('/update').patch(updateRoom);
+roomRouter.route('/user/add').post(addUserToRoom);
+roomRouter.route('/user/disconnect').post(disconnectUserFromRoom);
 
 export default roomRouter;
--- src/routes/v1/v1-main.js
+++ src/routes/v1/v1-main.js
@@ -6,8 +6,10 @@ import {default as questionRouter} from "./questions.routes.js"
 
 const router = express.Router();
 
+import { authVerify } from '../../middlewares/userAuth.middleware.js';
+
 router.use("/auth", userAuthRouter);
-router.use("/room", roomRouter);
-router.use("/questions",questionRouter);
+router.use("/room", authVerify, roomRouter);
+router.use("/questions", questionRouter);
 
 export default router;
